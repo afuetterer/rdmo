feab59e585d2c403e2cd87ab3058cae1f3927a40
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2023-07-30 09:27:49 +0200

    remove one middleware

--- testing/config/settings/base.py
+++ testing/config/settings/base.py
@@ -147,7 +147,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.sites.middleware.CurrentSiteMiddleware'
+    # 'django.contrib.sites.middleware.CurrentSiteMiddleware'
 ]
 
 import logging
